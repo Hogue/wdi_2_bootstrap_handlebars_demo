c1068268500a132b91241249531e77c34f7cf875
Max Blaushild <blaushmild@gmail.com>
2015-05-07 10:23:38 -0400

    finishes demo

--- app/scripts/characters.js
+++ app/scripts/characters.js
@@ -1,27 +1,29 @@
+"use strict";
+
 var characters = (function(){
 
   var getCharacters = function(){
     $.getJSON( "lib/characters.json").success(function(response){
       _renderCharacters(response.characters);
     });
-  }
+  };
 
   var _renderCharacters = function(characters){
     console.log(characters);
     // your code starts here
 
     // your code ends here
-  }
+  };
 
   return {
     indexCharacters: getCharacters
-  }
+  };
 
 })();
 
 $(document).ready(function(){
   characters.indexCharacters();
-})
+});
 
 
 
